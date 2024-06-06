3630efc92cb5eabb55b2d4702e216a9684d2458e
Carlos Eduardo <132010811+kadujms@users.noreply.github.com>
2024-06-06 14:43:53 -0300

    Update usuarios.js

--- API/routers/usuarios.js
+++ API/routers/usuarios.js
@@ -1,7 +1,9 @@
+
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/usuarios');
+const authgestor = require('../middleware/auth')
 
 router.get('/usuarios', userController.getAllUsers);
-router.delete('/usuarios/:email', userController.deleteUser)
+router.delete('/usuarios/:email', authgestor, userController.deleteUser)
 module.exports = router;
