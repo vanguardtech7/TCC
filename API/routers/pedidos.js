5dd6446d6dc96be3f5aac836357c047d50bf5262
Carlos Eduardo <132010811+kadujms@users.noreply.github.com>
2024-06-06 14:43:33 -0300

    Update pedidos.js

--- API/routers/pedidos.js
+++ API/routers/pedidos.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const authgestor = require('../middleware/auth')
 const pedidosControllers = require('../controller/pedidos'); 
 
 router.post('/pedidos', pedidosControllers.criarPedido);
 router.get('/meus-pedidos', pedidosControllers.listarPedidos);
 router.get('/pedidos', pedidosControllers.listarTodosOsPedidos);
-router.delete('/pedidos/:id', pedidosControllers.excluirPedido);
+router.delete('/pedidos/:id', authgestor, pedidosControllers.excluirPedido);
 router.delete('/meus-pedidos/:id', pedidosControllers.excluirMeuPedido);
 
 module.exports = router;
